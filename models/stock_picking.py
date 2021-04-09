f5377339e0aedeb70f8b3d1409b7505484373a84
rocketgithub <rodrigo@solucionesprisma.com>
2021-04-09 10:05:18 -0600

    Mejores validaciones

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -13,7 +13,7 @@ class StockPicking(models.Model):
     
     def _compute_show_validate(self):
         for picking in self:
-            if not self.user_has_groups('stock.group_stock_manager') and picking.picking_type_code != 'internal':
+            if not self.user_has_groups('stock.group_stock_manager') and self.env.user.default_location_id.id == self.location_dest_id.id:
                 picking.show_validate = False
                 return
                 
@@ -28,21 +28,12 @@ class StockPicking(models.Model):
         usuario_inventario = self.user_has_groups('stock.group_stock_user')
         responsable_inventario = self.user_has_groups('stock.group_stock_manager')
 
-        if self.picking_type_id.code == 'internal':
-            if usuario_inventario and not responsable_inventario:
+        if usuario_inventario and not responsable_inventario:
+            if self.picking_type_id.code == 'internal':
                 for l in self.move_line_ids_without_package:
                     if l.lot_id and l.lot_id.name:
                         quants = self.env['stock.quant'].search([('lot_id', '=', l.lot_id.id), ('location_id','=',l.location_id.id), ('product_id','=',l.product_id.id), ('quantity','>=',1)])
                         if not quants:
-                            raise UserError(_('La serie {} no existe en la ubicación de origen'.format(l.lot_id.name)))
+                            raise UserError('La serie {} no existe en la ubicación de origen'.format(l.lot_id.name))
 
         res = super(StockPicking, self).button_validate()
-
-        if self.picking_type_id.code == 'internal':
-            if responsable_inventario:
-                return res
-            elif usuario_inventario:
-                if self.env.user.default_location_id.id == self.location_dest_id.id and self.env.user.default_location_id.usage == 'internal':
-                    return res
-                else:
-                    raise UserError(_('El usuario debe de tener la misma ubicación que el envío'))
